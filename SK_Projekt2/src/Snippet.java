c019bfc3af11413fcaade6f5653f2611852af128
AntonHul <01154147@pw.edu.pl>
2022-04-18 18:41:36 +0200

    save list of files, calculate checksum

---
+++ SK_Projekt2/src/Snippet.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Snippet {
+	public static String hashFile(File file)
+	        throws NoSuchAlgorithmException, FileNotFoundException, IOException {
+	    // Set your algorithm
+	    // "MD2","MD5","SHA","SHA-1","SHA-256","SHA-384","SHA-512"
+	    MessageDigest md = MessageDigest.getInstance("SHA-512");
+	    FileInputStream fis = new FileInputStream(file);
+	    byte[] dataBytes = new byte[1024];
+	
+	    int nread = 0;
+	    while ((nread = fis.read(dataBytes)) != -1) {
+	        md.update(dataBytes, 0, nread);
+	    }
+	
+	    byte[] mdbytes = md.digest();
+	
+	    StringBuilder sb = new StringBuilder();
+	    for (int i = 0; i < mdbytes.length; i++) {
+	        sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+	    }
+	    return sb.toString();
+	}
+}
--- SK_Projekt2/src/UDPClient.java
+++ SK_Projekt2/src/UDPClient.java
@@ -1,13 +1,37 @@
+import java.io.File;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketTimeoutException;
+import java.nio.file.Files;
+import java.security.NoSuchAlgorithmException;
+import javax.swing.JFileChooser;
+
 
 public class UDPClient {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
  
+    	// allow client to choose directory
+        JFileChooser f = new JFileChooser();
+        f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
+        f.showSaveDialog(null);
+        // show chosen directory in console
+        System.out.println(f.getSelectedFile());
+        
+        // save all filenames from the chosen directory
+        File folder = new File(f.getSelectedFile().toString());
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+          if (listOfFiles[i].isFile()) {
+            System.out.println("File " + listOfFiles[i].getName());
+            //calculate the checksum using external lib hash
+            System.out.println("SHA512 : " + Snippet.hashFile(listOfFiles[i]));
+          }
+        }
+       
         String message = "tekst";
         InetAddress serverAddress = InetAddress.getByName("localhost");
         System.out.println(serverAddress);
@@ -29,5 +53,7 @@ public class UDPClient {
         }catch (SocketTimeoutException ste){
             System.out.println("Serwer nie odpowiedzia³, wiêc albo dosta³ wiadomoœæ albo nie...");
         }
+        
+        
     }
 }
